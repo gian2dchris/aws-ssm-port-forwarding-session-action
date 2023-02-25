37e9b65a9989970a626bcfa447fda260b0932a4d
Giannis Christinakis <50832176+gian2dchris@users.noreply.github.com>
2023-02-25 15:41:44 +0200

    index.js startSession

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { SSMClient, StartSessionCommand } = require("@aws-sdk/client-ssm");
 
-
-// most @actions toolkit packages have async methods
-async function run() {
+const run = async () => {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+    console.log("Start SSM Port Forwarding Session");
+    const client = new SSMClient({
+			region: process.env.AWS_DEFAULT_REGION,
+			customUserAgent: "gha".concat("-",github.context.repo.repo)
+	});
+    
+    const params = SanitizeInputs();
+    const command = new StartSessionCommand(params);
+ 
+    const data = await client.send(command);
+    core.saveState("session-id", data.SessionId);
+  } catch (err) {
+    console.error(err, err.stack);
+    core.setFailed(err.message);
   }
+};
+
+function SanitizeInputs() {
+
+  const _targetId = core.getInput('target-id', { required: true });
+  const _portNumber = core.getInput('portNumber', { required: true });
+  const _localPortNumber = core.getInput('localPortNumber', { required: true });
+  
+  const _documentName = 'AWS-StartPortForwardingSession';
+
+  return {
+    Target: _targetId,
+    DocumentName: _documentName,
+    Parameters: {
+      'portNumber': [
+        _portNumber,
+      ],
+      'localPortNumber': [
+        _localPortNumber,
+      ]
+    },
+    Reason: github.context.serverUrl + "/" + github.context.repo + "/actions/runs/" + github.context.runId.toString()
+  };
 }
 
 run();
